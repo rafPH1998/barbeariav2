048b670ec2a0fc4452cf387c4b87fc7dbd0ed31e
Rafael Belchior <rafaelb89.rb@gmail.com>
2023-07-25 11:31:09 -0300

    att

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,7 @@
       rel="stylesheet"
       href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
     />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     @routes
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
